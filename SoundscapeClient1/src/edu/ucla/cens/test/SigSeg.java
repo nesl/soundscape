86d4f2825b3e46f3e37b4bdaeaf1b157e5075694
adparker <adparker@c6739fea-bb1f-0410-b6c3-ac36619438dd>
2006-10-17 02:01:11 +0000

    Saving work. Working on upload.

--- SoundscapeClient1/src/edu/ucla/cens/test/SigSeg.java
+++ SoundscapeClient1/src/edu/ucla/cens/test/SigSeg.java
@@ -6,7 +6,12 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class SigSeg {
+import com.sun.midp.io.Base64;
+
+import edu.ucla.cens.test.SensorBaseInterface;
+
+public class SigSeg implements SensorBaseInterface {
+	int id = -1;
 	long timeMS = 0;
 	public byte[] data = null;
 	
@@ -17,12 +22,14 @@ public class SigSeg {
 		return;
 	}
 	
-	SigSeg (byte[] record) throws IOException
+	// TODO need to get passed in the record id, and assign it.
+	SigSeg (int id, byte[] record) throws IOException
 	{
 		ByteArrayInputStream byteIn = new ByteArrayInputStream(record);
 		DataInputStream dataIn = new DataInputStream(byteIn);
 		try
 		{
+			this.id = id;
 			this.timeMS = dataIn.readLong();
 			dataIn.read(this.data);
 			dataIn.close();
@@ -56,4 +63,29 @@ public class SigSeg {
 		}
 	}
 	
+	public String toXML() 
+	{
+		String result = "";
+		// <row>\n
+		result += "<row>\n";
+		// <field name="id">3434</field>\n
+		result += createFieldString("id", String.valueOf(this.id));
+		// <field name="date">10235135</date>\n
+		result += this.createFieldString("date", String.valueOf(this.timeMS));
+		// <field name="data">124af353d33c341....</data>\n
+		String b64enc = Base64.encode(this.data, 0, this.data.length);
+		// TODO implement URLEncode.encode
+		String urlenc = URLEncode.encode(b64enc); 
+		result += this.createFieldString("data", urlenc);
+		// </row>\n
+		result += "</row>\n";
+		return result;
+	}
+	
+	private String createFieldString(String name, String fieldVal)
+	{
+		String result = "";
+		result += "<field name=\"" + name + "\">" + fieldVal + "</field>\n";
+		return result;
+	}
 }
